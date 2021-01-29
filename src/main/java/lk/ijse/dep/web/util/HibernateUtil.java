5a59689764f451aa3b2ea6439feb4e9280ba9765
Tharanga Mahavila <tharangamahavila@gmail.com>
2021-01-29 23:46:40 +0530

    create HibernateUtil

--- src/main/java/lk/ijse/dep/web/util/HibernateUtil.java
+++ src/main/java/lk/ijse/dep/web/util/HibernateUtil.java
@@ -1,9 +1,16 @@
 package lk.ijse.dep.web.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author:Tharanga Mahavila <tharangamahavila@gmail.com>
  * @since : 2021-01-29
  **/
 public class HibernateUtil {
+    private static Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
+
+    EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("objectdb:myDbFile.odb");
 
 }
