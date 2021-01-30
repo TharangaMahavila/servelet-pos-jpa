94431ee6ca6028c2c2d4751b9aed1fd69a0fd51a
Tharanga Mahavila <tharangamahavila@gmail.com>
2021-01-30 07:36:11 +0530

    finish uni directional in OrderDetail Entity

--- src/main/java/lk/ijse/dep/web/entity/OrderDetail.java
+++ src/main/java/lk/ijse/dep/web/entity/OrderDetail.java
@@ -18,9 +18,21 @@ public class OrderDetail implements SuperEntity {
     private int qty;
     @Column(name = "unit_price")
     private BigDecimal unitPrice;
+    @ManyToOne
+    @JoinColumn(name = "order_id",referencedColumnName = "id",insertable = false,updatable = false)
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "item_code",referencedColumnName = "code",insertable = false,updatable = false)
+    private Item item;
 
     public OrderDetail(String orderId, String itemCode, int qty, BigDecimal unitPrice) {
         this.orderDetailPK = new OrderDetailPK(orderId,itemCode);
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public OrderDetail(OrderDetailPK orderDetailPK, int qty, BigDecimal unitPrice) {
+        this.orderDetailPK = orderDetailPK;
         this.qty = qty;
         this.unitPrice = unitPrice;
     }
